068eeeb2811442cb16f05d0d9184b353d0575453
Naru <mrnarukodo@gmail.com>
2020-05-14 20:48:54 -0400

    Add day 1 solution

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# adventofcode2019
-My own solutions for Advent of Code 2019 (Started May 14 2020)
+# Advent of Code 2019
+My own solutions for Advent of Code 2019 (Started May 14, 2020)
+Solutions uploaded every day except for Sundays.
+Inputs ignored
+
+Solution explanations will be typed here if the answer isn't trivial.
---
+++ day1/p1.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+import math
+
+def calc_fuel(mass):
+    fuel = math.floor(mass/3) - 2
+    total_fuel = fuel
+    while fuel > 5:
+        fuel = math.floor(fuel/3) - 2
+        total_fuel += fuel
+    return total_fuel
+
+
+with open(Path() / 'p1.input') as inputs:
+    total_mass_p2 = 0
+    total_mass_p1 = 0
+    for mass in inputs:
+        total_mass_p1 += math.floow(int(mass)/3) - 2
+        total_mass_p2 += calc_fuel(int(mass))
+
+print(total_mass_p1)
+print(total_mass_p2)
